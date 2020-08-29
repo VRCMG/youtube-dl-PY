6d44367d1cfa58adbdbc844f746c338e31002013
catnip <55117818+cutielovesyou@users.noreply.github.com>
2020-08-29 00:30:55 -0500

    run this.
    im gae

---
+++ run.bat
@@ -0,0 +1,12 @@
+@echo off
+chcp 65001
+title catnip's youtube mp3 downloader
+cls
+echo.
+echo      [1;33m╔═════════╗[0m
+echo      ║ [1;31mY T D L [0m║
+echo      [1;33m╚═════════╝[0m
+echo.
+echo loading...
+py retard.py
+pause
